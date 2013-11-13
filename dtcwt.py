22fca7c1aa785ec34953b9a9d8a58b036a9b4e7c
tim <timothy.daniel.roberts@gmail.com>
2013-11-13 00:53:06 +0000

    some message

--- dtcwt.py
+++ dtcwt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -tt
 import dtcwt
 import py_utils.signal_utilities
-class dtcwt(Operator):
+class Dtcwt(Operator):
     """
     """
     
@@ -19,46 +19,39 @@ class dtcwt(Operator):
         """
         if not self.lgc_adjoint:
             ary_size = size(mutiplicand)
-            int_dimension = multiplicand.shape[0]
+            int_dimension = multiplicand.ndim
             if int_dimension==1:
-                Yl,Yh = dtwavexfm(multiplicand,self.dict_section['nlevels'], \
-                                self.dict_section['biort'],self.dict_section['qshift'])
+                ary_scaling,tup_coeffs = dtwavexfm(multiplicand, \
+                                                     self.dict_section['nlevels'], \
+                                                     self.dict_section['biort'], \
+                                                     self.dict_section['qshift'])
             elif int_dimension==2:
-                Yl,Yh = dtwavexfm2(multiplicand,self.dict_section['nlevels'], \
-                                self.dict_section['biort'],self.dict_section['qshift'])
+                ary_scaling,tup_coeffs = dtwavexfm2(multiplicand, \
+                                                     self.dict_section['nlevels'], \
+                                                     self.dict_section['biort'], \
+                                                     self.dict_section['qshift'])
             else:
-                Yl,Yh = dtwavexfm3(multiplicand,self.dict_section['nlevels'], \
-                                self.dict_section['biort'],self.dict_section['qshift'], \
-                                discard_level_1=False)
-            multiplicand=self.order_subbands(Yl,Yh,ary_size)
+                ary_scaling,tup_coeffs = dtwavexfm3(multiplicand, \
+                                                     self.dict_section['nlevels'], \
+                                                     self.dict_section['biort'], \
+                                                     self.dict_section['qshift'] \
+                                                     self.dict_section['discard_level_1'])
+                multiplcand = WS(ary_scaling,tup_coeffs)                   
         else:
             ary_size = mutiplicand.ary_size
             int_dimension = mutiplicand.int_dimension
-            Yl,Yh = self.reorder_subbands(multiplicand.get_subbands(), \
-                                        self.dict_section['nlevels'],ary_size)
+            ary_scaling = multiplicand.ary_scaling
+            tup_coeffs = multiplicand.tup_coeffs
             if int_dimension==1:
-                multiplicand = dtwaveifm(Yl,Yh, \
-                                  self.dict_section['biort'],self.dict_section['qshift'])
+                multiplicand = dtwaveifm(ary_scaling,tup_coeffs, \
+                                         self.dict_section['biort'],self.dict_section['qshift'])
             elif int_dimension==2:
-                multiplicand = dtwaveifm2(Yl,Yh, \
+                multiplicand = dtwaveifm2(ary_scaling,tup_coeffs, \
                                           self.dict_section['biort'], \
                                           self.dict_section['qshift'])
             else:
-                multiplicand = dtwavexfm3(multiplicand, \
+                multiplicand = dtwaveifm3(ary_scaling,tup_coeffs, \
                                           self.dict_section['biort'], \
                                           self.dict_section['qshift'], \
                                           discard_level_1=False)
         return  super(Operator,self).__mul__(multiplicand)
-
-    def order_subbands(self,Yl,Yh,ary_size)
-        """
-        Reorder the DTCWT coefficient subbands into cell array 
-        for efficient array processing and indexing.
-        """
-        int_levels = len(Yh)
-        if len(Yh[1])==7:
-            int_dimension = 3
-            subbands_per_level = 28
-            ary_size = size(Yl)*2^(int_levels-1)
-        else:
-            if 
