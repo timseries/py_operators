74ca31ac31d933a2d057418941457b8fab3da84d
tim <timothy.daniel.roberts@gmail.com>
2013-11-24 23:14:39 +0000

    finished composition of operators (debugging)

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from op_dtcwt import DTCWT
+from op_blur import Blur
--- operator_comp.py
+++ operator_comp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -tt
 from numpy import arange
 from py_utils.section import Section
-from py_utils.section_factory import SectionFactory as sf
+from py_utils.section_factory import SectionFactory as sf 
 from py_operators.operator import Operator
 
 class OperatorComp(Operator):
@@ -14,9 +14,9 @@ class OperatorComp(Operator):
         Class constructor for Operator.
         """       
         super(OperatorComp,self).__init__(ps_parameters,str_section)
-        self.operator_names = self.get_val('operators',False)
-        self.ls_operators = [sf.create_section(ps_parameters,self.operator_names[i]) \
-          for i in arange(len(self.operator_names))] #build the operator objects
+        self.ls_operator_names = self.get_val('operators',False)
+        self.ls_operators = [sf.create_section(ps_parameters,self.ls_operator_names[i]) \
+          for i in arange(len(self.ls_operator_names))] #build the operator objects
         self.str_eval = self.get_mult_eval(False)  
         self.str_eval_adjoint = self.get_mult_eval(True)    
         self.str_eval_f = self.get_mult_eval(False,'.get_spectrum()')  
@@ -40,7 +40,7 @@ class OperatorComp(Operator):
         if lgc_adjoint:
             self.ls_operators.reverse()
             str_adjoint = '~'
-        str_eval = ''.join([str_adjoint + 'self.ls_operators[' + str(i) + ']' + method \
+        str_eval = ''.join([str_adjoint + 'self.ls_operators[' + str(i) + ']' + method + \
                             ' * ' for i in arange(len(self.ls_operators))])
         if method == '': #assume we want to multiply
             str_eval += 'multiplicand'               
